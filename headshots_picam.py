2d9ad3467b6cf3965f6a804a372168617121dea0
DavidAtkins55 <caramelapplemac@gmail.com>
2024-03-08 10:25:10 +0000

    first commit

--- headshots_picam.py
+++ headshots_picam.py
@@ -1,35 +1,37 @@
 import cv2
-from picamera import PiCamera
-from picamera.array import PiRGBArray
+# Import libraries for image processing (optional)
+# import numpy as np
 
-name = 'Caroline' #replace with your name
+name = 'Moron'  # Replace with your name
 
-cam = PiCamera()
-cam.resolution = (512, 304)
-cam.framerate = 10
-rawCapture = PiRGBArray(cam, size=(512, 304))
-    
-img_counter = 0
+# Define video capture object with device index (might need adjustment)
+cap = cv2.VideoCapture(0)  # Change index if your camera has a different ID
 
 while True:
-    for frame in cam.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        image = frame.array
-        cv2.imshow("Press Space to take a photo", image)
-        rawCapture.truncate(0)
-    
-        k = cv2.waitKey(1)
-        rawCapture.truncate(0)
-        if k%256 == 27: # ESC pressed
-            break
-        elif k%256 == 32:
-            # SPACE pressed
-            img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
-            cv2.imwrite(img_name, image)
-            print("{} written!".format(img_name))
-            img_counter += 1
-            
-    if k%256 == 27:
-        print("Escape hit, closing...")
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Check if frame is read correctly
+    if not ret:
+        print("Error: Unable to capture frame")
         break
 
+    # Display captured frame
+    cv2.imshow("Press Space to take a photo", frame)
+
+    # Capture key press
+    k = cv2.waitKey(1)
+
+    # Handle key presses
+    if k % 256 == 27:  # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k % 256 == 32:  # SPACE pressed
+        img_name = "dataset/" + name + "/image_{}.jpg".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+# Release the capture and close windows
+cap.release()
 cv2.destroyAllWindows()
